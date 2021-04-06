365f9f837b057f98b7b349516469d3f16808d9e8
stephenfranz22@gmail.com <stephenfranz22@gmail.com>
2021-04-06 19:34:02 -0400

    magic eight ball complete

--- mixed.js
+++ mixed.js
@@ -1,23 +1,47 @@
-let phrases = ['As I see it yes',
-'Ask again later',
-'Better not tell you now',
-'Cannot predict now',
-'Concentrate and ask again',
-'Don’t count on it',
-'It is certain',
-'It is decidedly so',
-'Most likely',
-'My reply is no',
-'My sources say no',
-'Outlook good',
-'Outlook not so good',
-'Reply hazy try again',
-'Signs point to yes',
-'Very doubtful',
-'Without a doubt',
-'Yes',
-'Yes, definitely',
-'You may rely on it'];
+let phrases = [
+  "As I see it yes",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don’t count on it",
+  "It is certain",
+  "It is decidedly so",
+  "Most likely",
+  "My reply is no",
+  "My sources say no",
+  "Outlook good",
+  "Outlook not so good",
+  "Reply hazy try again",
+  "Signs point to yes",
+  "Very doubtful",
+  "Without a doubt",
+  "Yes",
+  "Yes, definitely",
+  "You may rely on it",
+];
+console.log("\033[2J");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-
-
+readline.question(`What's your name?`, (name) => {
+  console.log(`Hello ${name}`);
+  console.log("Clear your mind and ask me a yes or no question....");
+  setTimeout(function () {
+    console.log("Let me think....");
+  }, 2000);
+  setTimeout(function () {
+    console.log("....");
+  }, 2000);
+  setTimeout(function () {
+    console.log("....");
+  }, 3000);
+  setTimeout(function () {
+    let randomIndex = Math.floor(Math.random() * phrases.length);
+    let response = phrases[randomIndex];
+    console.log(response);
+  }, 4000);
+  readline.close();
+});
